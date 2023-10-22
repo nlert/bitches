6d527c12c16f0e4e12096385516230e443ce9cd3
nlert <neillertola@gmail.com>
2023-10-22 18:30:48 -0400

    git commit index.html styles.css dark-theme.css light-theme.css index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+// Variables
 const names = document.querySelector(".name")
 const reveal = document.querySelector(".reveal")
 const button = document.querySelector(".bitches-btn")
 let bitchesVal = document.querySelector(".bitches-val")
+
+// Theme Button
+const sun = document.getElementById("sun")
+const moon = document.getElementById("moon")
+const themeStyle = document.getElementById("theme-style")
+
+sun.addEventListener('click', function() {
+    moon.classList.remove("theme-hide");
+    moon.classList.add("theme");
+
+    sun.classList.remove("theme");
+    sun.classList.add("theme-hide");
+
+    themeStyle.href = "light-theme.css";
+})
+
+moon.addEventListener('click', function() {
+    sun.classList.remove("theme-hide");
+    sun.classList.add("theme");
+
+    moon.classList.remove("theme");
+    moon.classList.add("theme-hide");
+
+    themeStyle.href = "dark-theme.css";
+})
+
+// Name Buttons
 let frank = document.querySelector(".frank")
 let jack = document.querySelector(".jack")
 let will = document.querySelector(".will")
@@ -12,13 +40,13 @@ let luan = document.querySelector(".luan")
 let baylor = document.querySelector(".baylor")
 
 frank.addEventListener('click', function() {
-    reveal.classList.add("reveal-reveal");
-    names.textContent = "Frank,"
-    bitchesVal.textContent = ""
-    window.scrollTo(0, document.body.scrollHeight);
+    reveal.classList.add("reveal-reveal"); // Reveal textbox
+    names.textContent = "Frank,"; // Add name to welcome statement
+    bitchesVal.textContent = ""; // Clear textbox 
+    window.scrollTo(0, document.body.scrollHeight); // Scroll to bottom of page
 
-    button.addEventListener('click', function() {
-        bitchesVal.textContent = "1 (giga_chad.png)"
+    button.addEventListener('click', function() { // Add text to textbox
+        bitchesVal.textContent = "1 (giga_chad.png)";
     })
 })
 
